bee0f22f87d34907fa2cdbde95e07522f4598ed5
jbarco108 <143366653+jbarco108@users.noreply.github.com>
2024-04-01 14:07:13 -0400

    Update MessageClassInterface.java

--- MessageClassInterface.java
+++ MessageClassInterface.java
@@ -1,18 +1,25 @@
 /**
  * User
  * <p>
- * Interface for the message class, contains all of the basic methods we will implement
+ * Interface for the message class, contains all the basic methods we will implement
  *
  * @author Chethan Adusumilli, cadusumi@purdue.edu
  * @version 1.0
  **/
 public interface MessageClassInterface {
-    String getMessageToBeSent();
-    User getReciever();
-    User getSender();
-    void setMessageToBeSent(String messageToBeSent);
-    void setSender(User sender);
-    void setReciever(User reciever);
-    void setSendSuccessful(boolean sendSuccessful);
-    String toMessageFileString();
+    String getMessageToBeSent();//content of message
+
+    User getReceiver();//retrieve receiver
+
+    User getSender(); //retrive sender
+
+    void setMessageToBeSent(String messageToBeSent); //set content
+
+    void setSender(User sender); //set sender
+
+    /* void setReciever(User reciever);*/ //method later to be implemented
+
+    void setSendSuccessful(boolean sendSuccessful);//boolean condition for messages
+
+    String toMessageFileString();//display the message as a string
 }
