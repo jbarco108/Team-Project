0dbe408f6c780ed55cfbffbb3b5c8c7c3b0778ec
jbarco108 <143366653+jbarco108@users.noreply.github.com>
2024-04-01 14:05:48 -0400

    Update Message.java

--- Message.java
+++ Message.java
@@ -1,14 +1,12 @@
 /**
  * Message
- *
+ * <p>
  * Message class containing recipient, sender, and message sent
  *
  * @author Alston Zhang, zhan4508@purdue.edu
- *
  * @version 1.0
- *
  */
-public class Message {
+public class Message implements MessageClassInterface {
     private String messageToBeSent; // the actual message itself
     boolean sendSuccessful; //return value of whether the message is sent successfully or not
     private User sender; //user class ; who sent the message
@@ -20,6 +18,7 @@ public class Message {
         this.sender = null;
         this.receiver = null;
     }
+
     public Message(User sender, User receiver, String message) { //intended constructor.
         this.sendSuccessful = true;
         this.messageToBeSent = message;
@@ -27,24 +26,32 @@ public class Message {
         this.receiver = receiver;
     }
 
+    //Getters for message class, retrieve the messageToBeSent, the receiver, and sender
     public String getMessageToBeSent() {
         return messageToBeSent;
     }
+
     public User getReceiver() {
         return receiver;
     }
+
     public User getSender() {
         return sender;
     }
+
+    //Setters for the message class, set the messageToBeSent, the sender and receiver and a boolean to indicate
+    //if it was successful
     public void setMessageToBeSent(String messageToBeSent) {
         this.messageToBeSent = messageToBeSent;
     }
+
     public void setSender(User sender) {
         this.sender = sender;
     }
-    public void setReceiver(User reciever) {
-        this.receiver = reciever;
-    }
+
+    /* public void setReceiver(User reciever) {
+         this.receiver = reciever;
+    }*/
     public void setSendSuccessful(boolean sendSuccessful) {
         this.sendSuccessful = sendSuccessful;
     }
