566cf9b4a83db274605fb02792cc23ee4ca6a3bc
jbarco108 <143366653+jbarco108@users.noreply.github.com>
2024-04-01 15:05:10 -0400

    Update Platform.java

--- Platform.java
+++ Platform.java
@@ -16,17 +16,17 @@ public interface Platform {
 
     void storeUser(User newUser) throws IOException; //store the user in text file
 
-    void addFriend(String username, String friendUsername) throws IOException;//adds friend to specific user array
+    void addFriend(String username, String friendUsername) throws IOException; //adds friend to specific user array
 
-    void removeFriend(String username, String friendUsername) throws IOException;//removes friend from specific user
+    void removeFriend(String username, String friendUsername) throws IOException; //removes friend from specific user
 
     // array
-    void blockUser(String username, String blockUsername) throws IOException;// add user to blocked array
+    void blockUser(String username, String blockUsername) throws IOException; // add user to blocked array
 
     // Message methods
-    void storeMessage(Message newMessage) throws IOException;//add message to message array and write in text file
+    void storeMessage(Message newMessage) throws IOException; //add message to message array and write in text file
 
-    void viewMessages(String username);//reviews text file to retrieve user specific messages
+    void viewMessages(String username); //reviews text file to retrieve user specific messages
 
     //delete message from text file and from user array
     void deleteMessage(String senderUsername, String receiverUsername, String messageContent) throws IOException;
@@ -35,9 +35,9 @@ public interface Platform {
     // Other methods
     User login(String username, String password); //set the current user as user object already present in text file
 
-    User findUserByUsername(String username);//find user in users array
+    User findUserByUsername(String username); //find user in users array
 
-    void rewriteUserFile() throws IOException;//rewrites the user objects in text files
+    void rewriteUserFile() throws IOException; //rewrites the user objects in text files
 
-    void rewriteMessageFile() throws IOException;//rewrites the message object in text files
+    void rewriteMessageFile() throws IOException; //rewrites the message object in text files
 }
